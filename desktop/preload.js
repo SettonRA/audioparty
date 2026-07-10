3cd1208781b04687f87edec56415ed1b184aa62e
SettonRA <clark@tech-ra.net>
2026-07-10 13:42:35 -0600

    Remove Electron desktop app (per-app audio capture not feasible via Chromium); will rebuild as native .NET app

--- desktop/preload.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  getAudioSources: () => ipcRenderer.invoke('get-audio-sources'),
-  getVersion:      () => ipcRenderer.invoke('get-version'),
-  platform: process.platform
-});
